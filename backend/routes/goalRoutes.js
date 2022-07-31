aa2b7f29db766869d5abb178af114b681bd09cb3
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2022-07-31 14:08:42 +0200

    goalRoutes further cleanup

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -9,15 +9,12 @@ const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/go
 //   D -> Delete
 
 // get is used to read content from the backend
-router.get("/", getGoals);
-
 // post is used to create/add content to the backend
-router.post("/", setGoal);
+// This is abbreviation is possible because both requests use the same route path
+router.route("/").get(getGoals).post(setGoal);
 
 // put is used to update content in the backend
-router.put("/:id", updateGoal);
-
 // delete is used to delete content in the backend
-router.delete("/:id", deleteGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
