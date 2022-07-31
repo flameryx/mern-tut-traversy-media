46080994d2105eeade03fc50261d93d02dca473f
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2022-07-31 18:46:04 +0200

    Added errorHandler Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 // this is the common js import statement. import ... from ... is from ES2015 syntax, and is mostly used in the front-end
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
