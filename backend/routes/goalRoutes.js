d5937c08400931aa42e217c4f3bfa18fa8ff3b66
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2022-08-02 01:47:02 +0200

    Initial frontend template, with routes, sign up and login page

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
 // CRUD:
 //   C -> Create
@@ -11,10 +12,10 @@ const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/go
 // get is used to read content from the backend
 // post is used to create/add content to the backend
 // This is abbreviation is possible because both requests use the same route path
-router.route("/").get(getGoals).post(setGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
 
 // put is used to update content in the backend
 // delete is used to delete content in the backend
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
